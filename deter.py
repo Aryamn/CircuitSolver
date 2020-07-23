deaa2cc958e0804dc0f1a4273767b6fbe7b804e7
Aryaman <jainaryaman123@gmail.com>
2020-07-24 05:19:40 +0530

    Completed full Matrix inverse remaning

--- deter.py
+++ deter.py
@@ -4,7 +4,7 @@ n = int(input("Enter number of resistors ")) #no of resistors
 m = int(input("Enter number of voltage+current sources "))
 
 resis_neighbors = [[0 for x in range(n+m+1)]for y in range(n+m+1)]
-
+B =np.zeros(shape=(n+2,m+1))
 
 #for every line till n take input of resistors and values and its ends
 # Take note of current flow 
@@ -17,12 +17,22 @@ component_value = []
 component_firstnode = []
 component_secondnode = []
 
+t = 1
+
 for i in range(0,n+m):
     inp1,inp2,inp3,inp4,inp5 = input().split()
 
     if inp2=="R":
         resis_neighbors[int(inp4)][int(inp5)] = int(inp3)
         resis_neighbors[int(inp5)][int(inp4)] = int(inp3)
+
+    if inp2=="E":
+        if inp4!="1":
+            B[int(inp4)][t] = 1
+
+        if inp5!="1":    
+            B[int(inp5)][t] = -1
+        t+=1
 
     component_name.append(inp1)
     component_type.append(inp2)
@@ -42,5 +52,26 @@ for i in range(0,n):
             if(j!=ground):
                 G[i][j-2] -= conductance
 
+B = B[2:,1:]
+C = B.transpose()
+D = np.zeros(shape=(m,m))
 
-print(G) #Perfect Resistance Matrix
+final_matrix = np.zeros(shape=(n+m,n+m))
+
+for i in range(n):
+    for j in range(n):
+        final_matrix[i][j] = G[i][j]
+
+for i in range(n):
+    for j in range(m):
+        final_matrix[i][j+n] = B[i][j]
+
+for i in range(m):
+    for j in range(n):
+        final_matrix[i+n][j] = C[i][j]
+
+for i in range(m):
+    for j in range(m):
+        final_matrix[i+n][j+n] = D[i][j]
+
+print(final_matrix)
